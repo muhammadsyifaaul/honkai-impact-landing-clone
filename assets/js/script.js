acc8c1b2398d4daad7f94e6b73e3e6025ecd0396
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-02-08 01:47:47 +0700

    add project to github

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// const navHamburger = document.querySelector('.nav-hamburger');
+// const navLinks = document.querySelector('.nav-links ul');
+
+// navHamburger.addEventListener('click', () => {
+//     if (navLinks.classList.contains('open')) {
+//         navLinks.style.transform = 'translateX(100%)';
+//         navLinks.style.transition = '0.5s';
+//         navLinks.classList.remove('open');
+//     }
+//     else {
+//         navLinks.classList.add('open');
+//         navLinks.style.transform = 'translateX(0)';
+        
+//     }
+// });
+
